47b67f4df343a3759df46986e3a1358640d56224
dit99871@email.com <dit99871@gmail.com>
2025-08-20 08:00:18 +0400

    Обновление User

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -1,18 +1,38 @@
-import datetime as dt
+import datetime
+from enum import Enum
 from typing import Literal
 from uuid import uuid4
 
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-)
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .mixins import IntIdPkMixin
 from .base import Base
 
 
+class UserRole(Enum):
+    USER = "user"
+    ADMIN = "admin"
+    MODERATOR = "moderator"
+
+
+class KFALevel(Enum):
+    VERY_LOW = "1"
+    LOW = "2"
+    MEDIUM = "3"
+    HIGH = "4"
+    VERY_HIGH = "5"
+
+
+class GoalType(Enum):
+    LOSE_WEIGHT = "Снижение веса"
+    GAIN_WEIGHT = "Увеличение веса"
+    MAINTAIN_WEIGHT = "Поддержание веса"
+
+
 class User(IntIdPkMixin, Base):
-    uid: Mapped[str] = mapped_column(default_factory=lambda: str(uuid4()))
+    uid: Mapped[str] = mapped_column(
+        unique=True, index=True, default_factory=lambda: str(uuid4())
+    )
     username: Mapped[str] = mapped_column(unique=True, index=True)
     email: Mapped[str] = mapped_column(unique=True, index=True)
     hashed_password: Mapped[bytes]
@@ -21,12 +41,13 @@ class User(IntIdPkMixin, Base):
     age: Mapped[int | None] = mapped_column(nullable=True)
     weight: Mapped[float | None] = mapped_column(nullable=True)
     height: Mapped[float | None] = mapped_column(nullable=True)
-    kfa: Mapped[Literal["1", "2", "3", "4", "5"]] = mapped_column(nullable=True)
-    goal: Mapped[Literal["Снижение веса", "Увеличение веса", "Поддержание веса"]] = (
-        mapped_column(nullable=True)
-    )
+    kfa: Mapped[KFALevel | None] = mapped_column(nullable=True)
+    goal: Mapped[GoalType | None] = mapped_column(nullable=True)
 
     is_subscribed: Mapped[bool] = mapped_column(default=True)
-    is_active: Mapped[bool] = mapped_column(default=True)
-    role: Mapped[str] = mapped_column(default="user")
-    created_at: Mapped[str] = mapped_column(default=dt.datetime.now().date())
+    is_active: Mapped[bool] = mapped_column(default=True, index=True)
+    role: Mapped[UserRole] = mapped_column(default=UserRole.USER)
+
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        default_factory=lambda: datetime.datetime.now().date(), index=True
+    )
