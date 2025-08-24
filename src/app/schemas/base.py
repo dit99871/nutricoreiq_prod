a2bbdcb918c1bd54a4426e8a037b60f671e9b9b1
di <dit99871@gmail.com>
2025-08-24 12:55:56 +0400

    Редактировани схемы для включения csrf_токена

--- src/app/schemas/base.py
+++ src/app/schemas/base.py
@@ -8,3 +8,14 @@ class BaseSchema(BaseModel):
         use_enum_values=True,
         extra="forbid",
     )
+
+
+class FormSchema(BaseModel):
+    """Базовая схема для форм с CSRF-токеном."""
+    _csrf_token: str | None = None
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        use_enum_values=True,
+        extra="ignore",  # Разрешаем игнорировать лишние поля, но не добавлять их в модель
+    )
