fcd8f20da098e7ea08ead89a634fd2f0184a4acc
dit99871@email.com <dit99871@gmail.com>
2025-09-25 20:28:12 +0400

    Добавление исключений для сервисного слоя пользователя

--- src/app/core/exceptions.py
+++ src/app/core/exceptions.py
@@ -15,3 +15,19 @@ class ExpiredTokenException(HTTPException):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=detail,
         )
+
+
+class UserServiceError(Exception):
+    """Базовый класс для ошибок сервиса пользователей"""
+
+    pass
+
+
+class UserAlreadyExistsError(UserServiceError):
+    """Ошибка при попытке создать пользователя, который уже существует"""
+
+    status_code = status.HTTP_400_BAD_REQUEST
+
+    def __init__(self, message: str = "Пользователь с такими данными уже существует"):
+        self.detail = message
+        super().__init__(self.detail)
