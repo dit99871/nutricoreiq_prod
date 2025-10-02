9df02fc6dbabe09a3d6cdfd7db950a42277d73fe
dit99871@email.com <dit99871@gmail.com>
2025-10-02 13:09:19 +0400

    Создание алиасов для зависимостей

--- src/app/routers/auth.py
+++ src/app/routers/auth.py
@@ -17,7 +17,7 @@ from src.app.core import db_helper
 from src.app.core.config import settings
 from src.app.core.exceptions import ExpiredTokenException, UserAlreadyExistsError
 from src.app.core.logger import get_logger
-from src.app.core.redis import get_redis
+from src.app.core.redis import get_redis_service
 from src.app.core.services.limiter import limiter
 from src.app.core.services.user_service import UserService, get_user_service
 from src.app.core.utils.auth import create_response
@@ -26,10 +26,12 @@ from src.app.schemas.user import PasswordChange, UserCreate, UserPublic
 log = get_logger("auth_router")
 
 # алиасы типов зависимостей
-UserServiceDep = Annotated[UserService, Depends(get_user_service)]
-db_session = Annotated[AsyncSession, Depends(db_helper.session_getter)]
-current_user = Annotated[UserPublic, Depends(UserServiceDep.get_current_auth_user)]
-redis_dependency = Annotated[Redis, Depends(get_redis)]
+user_service_dep = Annotated[UserService, Depends(get_user_service)]
+db_session_dep = Annotated[AsyncSession, Depends(db_helper.session_getter)]
+current_user_dep = Annotated[
+    UserPublic, Depends(user_service_dep.get_user_by_access_jwt)
+]
+redis_service_dep = Annotated[Redis, Depends(get_redis_service)]
 
 router = APIRouter(
     tags=["Authentication"],
@@ -45,7 +47,7 @@ router = APIRouter(
 async def register_user(
     request: Request,
     user_in: UserCreate,
-    user_service: UserServiceDep,
+    user_service: user_service_dep,
 ) -> UserPublic:
     """
     Регистрирует нового пользователя в базе данных.
@@ -54,17 +56,15 @@ async def register_user(
     Если пользователь уже зарегистрирован, вызывает `HTTPException` со статусом 400
     и сообщением об ошибке.
 
-    :param request: Текущий объект запроса.
+    :param request: Объект текущего запроса.
     :param user_in: Данные пользователя для регистрации.
     :param user_service: Сервис для работы с пользователями, создается автоматически.
     :return: Зарегистрированный пользователь.
     :raises HTTPException: Если пользователь уже зарегистрирован.
     """
 
-    client_host = request.client.host if request.client else None
-
     try:
-        return await user_service.register_user(user_in, client_host)
+        return await user_service.register_user(user_in=user_in, request=request)
 
     except UserAlreadyExistsError as e:
         raise HTTPException(
@@ -78,8 +78,8 @@ async def register_user(
 async def login(
     request: Request,
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    session: db_session,
-    user_service: UserServiceDep,
+    session: db_session_dep,
+    user_service: user_service_dep,
 ) -> Response:
     """
     Аутентифицирует пользователя и возвращает response с access и refresh токенами.
@@ -88,7 +88,7 @@ async def login(
     access и refresh токены в случае успешной аутентификации. Использует OAuth2
     схему аутентификации.
 
-    :param request: Текущий объект запроса.
+    :param request: Объект текущего запроса.
     :param form_data: Данные формы, содержащие имя пользователя и пароль.
     :param session: Текущая сессия базы данных.
     :param user_service: Сервис для работы с пользователями.
@@ -99,6 +99,7 @@ async def login(
     """
 
     auth_user = await user_service.login(
+        request=request,
         session=session,
         username=form_data.username,
         password=form_data.password,
@@ -110,21 +111,21 @@ async def login(
 @router.post("/logout")
 async def logout(
     request: Request,
-    user: current_user,
-    redis: redis_dependency,
-    user_service: UserServiceDep,
+    user: current_user_dep,
+    redis_service: redis_service_dep,
+    user_service: user_service_dep,
 ) -> Response:
     """
     Выход пользователя из системы и инвалидация refresh токена.
 
-    :param request: Текущий объект запроса.
+    :param request: Объект текущего запроса.
     :param user: Аутентифицированный пользователь.
-    :param redis: Redis клиент для инвалидации refresh токена.
+    :param redis_service: Redis клиент для инвалидации refresh токена.
     :param user_service: Сервис для работы с пользователями.
     :return: Ответ с сообщением об успешном выходе.
     """
 
-    return await user_service.logout(request, redis, user)
+    return await user_service.logout(request, redis_service, user)
 
 
 @router.post(
@@ -133,25 +134,30 @@ async def logout(
 )
 async def refresh_token(
     request: Request,
-    session: db_session,
-    redis: redis_dependency,
-    user_service: UserServiceDep,
+    session: db_session_dep,
+    redis_service: redis_service_dep,
+    user_service: user_service_dep,
 ) -> Response:
+    """
+    Обновляет access и refresh токены пользователя.
 
-    refresh_jwt = request.cookies.get("refresh_token")
-    if not refresh_jwt:
-        log.error("Refresh token not found in cookies")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail={
-                "message": "Ошибка аутентификации. Пожалуйста, войдите заново",
-                "details": {
-                    "message": "Refresh token not found in cookies",
-                },
-            },
-        )
-    user = await user_service.get_current_auth_user_for_refresh(
-        refresh_jwt, session, redis
+    Принимает валидный refresh токен и возвращает новую пару access и refresh токенов.
+    Старый refresh токен помечается как использованный в Redis.
+
+    :param request: Объект текущего запроса.
+    :param session: Текущая сессия базы данных.
+    :param redis_service: Redis клиент для работы с refresh токенами.
+    :param user_service: Сервис для работы с пользователями.
+    :return: Response с новыми access и refresh токенами.
+    :raises HTTPException:
+        - 401: Если refresh токен невалидный или истек.
+        - 400: Если токен не передан или имеет неверный формат.
+    """
+
+    user = await user_service.get_user_by_refresh_jwt(
+        request=request,
+        session=session,
+        redis_service=redis_service,
     )
 
     return await create_response(user)
@@ -162,9 +168,9 @@ async def refresh_token(
 async def change_password(
     password_data: PasswordChange,
     request: Request,
-    user: current_user,
-    session: db_session,
-    user_service: UserServiceDep,
+    user: current_user_dep,
+    session: db_session_dep,
+    user_service: user_service_dep,
 ) -> Response:
     """
     Изменяет пароль аутентифицированного пользователя.
@@ -173,7 +179,7 @@ async def change_password(
     пользователя в системе. Требует аутентификации.
 
     :param password_data: Данные для смены пароля (текущий и новый пароль)
-    :param request: Объект запроса FastAPI
+    :param request: Объект текущего запроса
     :param user: Текущий аутентифицированный пользователь
     :param session: Сессия базы данных
     :param user_service: Сервис для работы с пользователями
@@ -185,4 +191,9 @@ async def change_password(
     if user is None:
         raise ExpiredTokenException()
 
-    return await user_service.change_password(session, user, password_data)
+    return await user_service.change_password(
+        request=request,
+        session=session,
+        user=user,
+        password_data=password_data,
+    )
