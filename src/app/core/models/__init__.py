fc97f125c3852d1a4db9ea2458002e8df353ef31
dit99871 <dit99871@email.com>
2026-01-25 11:59:18 +0400

    Создание модели для политики конфиденциальности + рефакторинг модели User

--- src/app/core/models/__init__.py
+++ src/app/core/models/__init__.py
@@ -1,6 +1,7 @@
 from .base import Base
 from .nutrient import Nutrient, NutrientCategory
 from .pending_product import PendingProduct
+from .privacy_consent import PrivacyConsent, ConsentType
 from .product import Product
 from .product_group import ProductGroup
 from .product_nutrient import ProductNutrient
@@ -9,6 +10,8 @@ from .utils.product import map_to_schema
 
 __all__ = [
     "Base",
+    "ConsentType",
+    "PrivacyConsent",
     "User",
     "PendingProduct",
     "Product",
