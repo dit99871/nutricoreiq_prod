bc7ff56c4a2c7442493771afa2eec7a0b6bf0091
dit99871 <dit99871@gmail.com>
2026-02-12 14:31:07 +0400

    Рефакторинг settings.py (путь к .env)

--- src/app/core/config/settings.py
+++ src/app/core/config/settings.py
@@ -22,28 +22,8 @@ from .taskiq import TaskiqConfig
 class Settings(BaseSettings):
     DEBUG: bool = False
 
-    # переменные для Docker Compose (без префикса)
-    POSTGRES_DB: str = ""
-    POSTGRES_USER: str = ""
-    POSTGRES_PASSWORD: str = ""
-    POSTGRES_HOST: str = ""
-    POSTGRES_PORT: str = ""
-    POSTGRES_SSL_MODE: str = ""
-    REDIS_PORT: str = ""
-    REDIS_ADDR: str = ""
-    REDIS_LOG_LEVEL: str = ""
-    HTTP_PORT: str = ""
-    HTTPS_PORT: str = ""
-    GF_SERVER_PROTOCOL: str = ""
-    GF_SERVER_DOMAIN: str = ""
-    GF_SERVER_ROOT_URL: str = ""
-    RABBITMQ_PORT: str = ""
-    RABBITMQ_MANAGEMENT_PORT: str = ""
-    RABBITMQ_DEFAULT_USER: str = ""
-    RABBITMQ_DEFAULT_PASS: str = ""
-
     model_config = SettingsConfigDict(
-        env_file=BASE_DIR / ".env",
+        env_file=BASE_DIR.parent.parent / ".env",
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
