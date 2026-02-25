32c80f86c74a4d3ac2436894a45ce6241af194ad
dit99871 <dit99871@gmail.com>
2026-02-25 20:29:21 +0400

    Рефакторинг настройки алембика (удаление логера)

--- src/app/alembic/env.py
+++ src/app/alembic/env.py
@@ -15,11 +15,6 @@ from src.app.core.models import (
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
