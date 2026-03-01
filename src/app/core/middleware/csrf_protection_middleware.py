b1bce4f57f3f53dde4a78bc194e1e7adcb81e567
dit99871 <dit99871@gmail.com>
2026-03-01 19:51:34 +0400

    Обратный рефакторинг путей, для которых не требуется csrf защита (для графаны)

--- src/app/core/middleware/csrf_protection_middleware.py
+++ src/app/core/middleware/csrf_protection_middleware.py
@@ -22,6 +22,7 @@ class CSRFProtectionMiddleware(BaseMiddleware):
         f"{settings.router.auth}/refresh",
         f"{settings.router.security}/csp-report",
         f"{settings.router.product}/pending",
+        "http://nutricoreiq.ru/apis/features.grafana.app/v0alpha1/namespaces/default/ofrep/v1/evaluate/flags",
     ]
 
     def __init__(
